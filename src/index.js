d722dbf815350c3ce922bf3e34dd006fdfec1114
Adrian Gutierrez <ir.da21@outlook.com>
2023-05-09 13:10:02 -0600

    modelos y base conectada

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import rutasCard from './routes/card.routes.js';
-//import { sequelize } from './database/database.js';
+import { sequelize } from './database/database.js';
 
-function main(){
+async function main(){
     //Manejo de excepciones
-    /*try{
+    try{
         await sequelize.sync({force:false});
         await sequelize.authenticate();
         console.log("Conexion exitosa");
     }catch(error){
         console.log("Conexion fallida, error -> "+error);
-    }*/
+    }
     const app = express();
     app.use(express.json()); 
     app.use(express.urlencoded({extended:false})); 
